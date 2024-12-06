f266fc202064b35164ba87992d9f1a5950dd6f42
marat0608 <marat0608@gmail.com>
2024-12-06 22:20:47 +0300

    Add files via upload

---
+++ Modul_5_3.py
@@ -0,0 +1,69 @@
+class House:
+    pass
+
+    def __init__(self, name, numbers_of_floors):
+        self.name = name
+        self.numbers_of_floors = numbers_of_floors
+    def __str__(self):
+        return str(f'Название: {self.name}, Количество этажей: {self.numbers_of_floors}')
+
+    def __eq__(self, other):
+        if isinstance(self.numbers_of_floors, int) and isinstance(other, House):
+            return self.numbers_of_floors == other.numbers_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.numbers_of_floors += value
+            return self
+
+    def __radd__(self, value):
+        if isinstance(value, int):
+            self.numbers_of_floors += value
+            return self
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.numbers_of_floors += value
+            return self
+
+    def __lt__(self, other):
+        if isinstance(other.numbers_of_floors, int) and isinstance(other, House):
+            return self.numbers_of_floors < other.numbers_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other.numbers_of_floors, int) and isinstance(other, House):
+            return self.numbers_of_floors >= other.numbers_of_floors
+
+    def __le__(self, other):
+        if isinstance(other.numbers_of_floors, int) and isinstance(other, House):
+            return self.numbers_of_floors <= other.numbers_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other.numbers_of_floors, int) and isinstance(other, House):
+            return self.numbers_of_floors != other.numbers_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other.numbers_of_floors, int) and isinstance(other, House):
+            return self.numbers_of_floors > other.numbers_of_floors
+
+
+h1 = House("ЖК Высота", 10)
+h2 = House("ЖК Айсберг", 20)
+print(h1)
+print(h2)
+print(h1 == h2)  # __eq__
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2)
+h1 += 10  # __iadd__
+print(h1)
+h2 = 10 + h2  # __radd__
+print(h2)
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
+
+
+
